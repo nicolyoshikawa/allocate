76a930be009a70da27c342cadda3f5090891bd1d
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-08-17 20:55:50 -0700

    added name

--- app/models/expenseGroup.py
+++ app/models/expenseGroup.py
@@ -8,7 +8,7 @@ class ExpenseGroup(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50))
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
