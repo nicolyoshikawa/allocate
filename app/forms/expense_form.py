4b58bb8a29848bea09f6911f56537142ce92e4da
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-08-19 14:15:47 -0700

    post expense

--- app/forms/expense_form.py
+++ app/forms/expense_form.py
@@ -6,7 +6,8 @@ from app.models import User
 class ExpenseForm(FlaskForm):
     description = StringField('Description', validators=[DataRequired(), Length(min=0, max=500)])
     # paid_by = StringField('Paid By', validators=[DataRequired()])
-    group_id = IntegerField('Group Id', validators=[DataRequired(), NumberRange(min=0)])
+    # group_id = IntegerField('Group Id', validators=[DataRequired(), NumberRange(min=0)])
     price = IntegerField('Price', validators=[DataRequired(), NumberRange(min=0)])
     receipt_img_url = StringField("Receipt Image", validators=[Length(min=0, max=255), URL(), Optional()])
     expense_date = DateField("Expense Date")
+    friend_id = IntegerField('Friend Id', validators=[DataRequired(), NumberRange(min=0)])
