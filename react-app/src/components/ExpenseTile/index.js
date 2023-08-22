6ee3c81c591efbdbb962edcc8304044a57219a12
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-08-21 23:37:21 -0700

    delete expense

--- react-app/src/components/ExpenseTile/index.js
+++ react-app/src/components/ExpenseTile/index.js
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 
 const ExpenseTile = ({expense, clickable}) => {
-    const amount_split = Number(expense.price/2).toFixed(2);
-    const date = new Date(expense.expense_date)
+    const amount_split = Number(expense?.price/2).toFixed(2);
+    const date = new Date(expense?.expense_date)
     const dateFormat = date.toLocaleDateString()
     let paid_by;
     let user_owes;
 
-    for(let i = 0; i < expense?.expense_group_users.length; i++){
+    for(let i = 0; i < expense?.expense_group_users?.length; i++){
         let group_user = expense.expense_group_users[i]
         if(group_user.id === expense.paid_by){
             paid_by = group_user.username;
@@ -16,20 +17,23 @@ const ExpenseTile = ({expense, clickable}) => {
             user_owes = group_user.username;
         }
     }
+
     return (
-        <div>
-            <div>{dateFormat}</div>
-            <div>{expense.receipt_img_url ?
-                    <img src={expense.receipt_img_url} alt="receipt_img"/>
-                    : <div></div>}
+        <>
+            <div>
+                <div>{dateFormat}</div>
+                <div>{expense?.receipt_img_url ?
+                        <img src={expense?.receipt_img_url} alt="receipt_img"/>
+                        : <div></div>}
+                </div>
+                {clickable ? (
+                    <div><Link to={`/expenses/${expense.id}`}> {expense?.description}</Link></div>
+                    ) : <div>{expense?.description}</div>
+                }
+                <div>{paid_by} paid ${expense?.price}</div>
+                <div>{user_owes} owes ${amount_split}</div>
             </div>
-            {clickable ? (
-                <div><Link to={`/expenses/${expense.id}`}> {expense.description}</Link></div>
-                ) : <div>{expense.description}</div>
-            }
-            <div>{paid_by} paid ${expense.price}</div>
-            <div>{user_owes} owes ${amount_split}</div>
-        </div>
+        </>
     )
 };
 
