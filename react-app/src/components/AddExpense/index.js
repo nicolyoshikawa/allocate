3ad7f916718e59734fb8aaebdaea63aafdd92766
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-08-29 20:32:19 -0700

    added constraint on friend id

--- react-app/src/components/AddExpense/index.js
+++ react-app/src/components/AddExpense/index.js
@@ -54,11 +54,12 @@ function AddExpense({expense, param_id}) {
             }
         })
     },[dispatch, expense?.id, expense?.receipt_img_url, expense?.description, expense?.price, expense?.group_id, expense?.expense_date, user_id]);
-
+    console.log(friend_id, hasSubmitted)
     useEffect(() => {
         const errors = [];
         if(description && description.length > 500) errors.push("Your description needs to be less than 500 characters");
         if(price && (price < 1)) errors.push("Price needs to be at least $1");
+        if(friend_id === "") errors.push("Please choose a friend to split with");
         if(receipt_img_url && (!receipt_img_url.endsWith(".png") &&
             !receipt_img_url.endsWith(".jpg") && !receipt_img_url.endsWith(".jpeg"))) {
             errors.push("Image URL must end in .png, .jpg, or .jpeg");
