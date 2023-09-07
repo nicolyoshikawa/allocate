d467327ef2079454583dcf555bbd0c870fc3472a
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-09-06 20:46:15 -0700

    landing page css

--- react-app/src/components/NavSideBar/index.js
+++ react-app/src/components/NavSideBar/index.js
@@ -7,8 +7,7 @@ function NavSideBar() {
     <div className="sidebar">
       <div className='all-expenses'>
         <NavLink activeClassName="selected" activeStyle={{ color: "#5bc5a7", fontWeight: "bold"}} exact to="/home">
-          <i class="fa-solid fa-ellipsis-vertical" style={{ color: "#808080", fontWeight: "bold" }}></i>
-          <i class="fa-solid fa-bars" style={{ color: "#808080" , fontWeight: "bold" }} ></i>
+          <i className="fa-solid fa-list" style={{ color: "#808080", fontWeight: "bold" }}></i>
           {" "}
           All Expenses
         </NavLink>
