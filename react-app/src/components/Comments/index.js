bc4613f4b166ef173f2de65eb3c55806c95fad3c
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-09-27 22:12:36 -0700

    edited comment feed

--- react-app/src/components/Comments/index.js
+++ react-app/src/components/Comments/index.js
@@ -3,12 +3,13 @@ import AddComment from "../AddComment";
 
 export default function Comments({expense}) {
     const comments_arr = expense.comments;
-    const sorted_comments = comments_arr?.sort((a,b) => new Date(b.created_at) - new Date(a.created_at))
+    // const sorted_comments = comments_arr?.sort((a,b) => new Date(b.created_at) - new Date(a.created_at))
 
     return (
         <div className="comments-container">
             <div className="comment"><i className="fa-solid fa-comment" style={{ color: "#808080" }}></i>NOTES AND COMMENTS</div>
-            {sorted_comments?.map(el => (<CommentsTile key={el.id} comments={el} expense={expense}/>))}
+            {/* {sorted_comments?.map(el => (<CommentsTile key={el.id} comments={el} expense={expense}/>))} */}
+            {comments_arr?.map(el => (<CommentsTile key={el.id} comments={el} expense={expense}/>))}
             <div className="comment">
                 <AddComment expense={expense}/>
             </div>
